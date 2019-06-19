de3ab01061fcd95d1d924ec01dc515e59fa17b3e
szymoff <50615156+szymoff@users.noreply.github.com>
2019-06-19 11:44:07 +0200

    Add files via upload

---
+++ form_exhibit_ids.php
@@ -0,0 +1,259 @@
+<?php
+/*
+Plugin Name: Form Exhibitors Code System
+Description: Wtyczka umożliwiająca generowanie kodów zaproszeniowych dla wystawców oraz tworzenie 'reflinków'.
+Version: 1.0.0
+Author: Szymon Kaluga
+Author URI: http://skaluga.pl/
+*/
+
+
+
+
+class PageTemplater {
+
+	/**
+	 * A reference to an instance of this class.
+	 */
+	private static $instance;
+
+	/**
+	 * The array of templates that this plugin tracks.
+	 */
+	protected $templates;
+
+	/**
+	 * Returns an instance of this class.
+	 */
+	public static function get_instance() {
+
+		if ( null == self::$instance ) {
+			self::$instance = new PageTemplater();
+		}
+
+		return self::$instance;
+
+	}
+
+	/**
+	 * Initializes the plugin by setting filters and administration functions.
+	 */
+	private function __construct() {
+
+		$this->templates = array();
+
+
+		// Add a filter to the attributes metabox to inject template into the cache.
+		if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) {
+
+			// 4.6 and older
+			add_filter(
+				'page_attributes_dropdown_pages_args',
+				array( $this, 'register_project_templates' )
+			);
+
+		} else {
+
+			// Add a filter to the wp 4.7 version attributes metabox
+			add_filter(
+				'theme_page_templates', array( $this, 'add_new_template' )
+			);
+
+		}
+
+		// Add a filter to the save post to inject out template into the page cache
+		add_filter(
+			'wp_insert_post_data',
+			array( $this, 'register_project_templates' )
+		);
+
+
+		// Add a filter to the template include to determine if the page has our
+		// template assigned and return it's path
+		add_filter(
+			'template_include',
+			array( $this, 'view_project_template')
+		);
+
+
+		// Add your templates to this array.
+		$this->templates = array(
+			'page-checkform.php' => 'Exhibitors Code Checker',
+			'page-registerme.php' => 'Exhibitors Code Maker'
+		);
+
+	}
+
+	/**
+	 * Adds our template to the page dropdown for v4.7+
+	 *
+	 */
+	public function add_new_template( $posts_templates ) {
+		$posts_templates = array_merge( $posts_templates, $this->templates );
+		return $posts_templates;
+	}
+
+	/**
+	 * Adds our template to the pages cache in order to trick WordPress
+	 * into thinking the template file exists where it doens't really exist.
+	 */
+	public function register_project_templates( $atts ) {
+
+		// Create the key used for the themes cache
+		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+
+		// Retrieve the cache list.
+		// If it doesn't exist, or it's empty prepare an array
+		$templates = wp_get_theme()->get_page_templates();
+		if ( empty( $templates ) ) {
+			$templates = array();
+		}
+
+		// New cache, therefore remove the old one
+		wp_cache_delete( $cache_key , 'themes');
+
+		// Now add our template to the list of templates by merging our templates
+		// with the existing templates array from the cache.
+		$templates = array_merge( $templates, $this->templates );
+
+		// Add the modified cache to allow WordPress to pick it up for listing
+		// available templates
+		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+
+		return $atts;
+
+	}
+
+	/**
+	 * Checks if the template is assigned to the page
+	 */
+	public function view_project_template( $template ) {
+		// Return the search template if we're searching (instead of the template for the first result)
+		if ( is_search() ) {
+			return $template;
+		}
+
+		// Get global post
+		global $post;
+
+		// Return template if post is empty
+		if ( ! $post ) {
+			return $template;
+		}
+
+		// Return default template if we don't have a custom one defined
+		if ( ! isset( $this->templates[get_post_meta(
+			$post->ID, '_wp_page_template', true
+		)] ) ) {
+			return $template;
+		}
+
+		// Allows filtering of file path
+		$filepath = apply_filters( 'page_templater_plugin_dir_path', plugin_dir_path( __FILE__ ) );
+
+		$file =  $filepath . get_post_meta(
+			$post->ID, '_wp_page_template', true
+		);
+
+		// Just to be safe, we check if the file exist first
+		if ( file_exists( $file ) ) {
+			return $file;
+		} else {
+			echo $file;
+		}
+
+		// Return template
+		return $template;
+
+	}
+
+}
+add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
+
+// create custom plugin settings menu
+add_action('admin_menu', 'my_cool_plugin_create_menu');
+
+function my_cool_plugin_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('Exhibitors Code System Plugin Settings', 'Exhibitors Code System Settings', 'administrator', __FILE__, 'exhibitors_options_page' , plugins_url('icon_small.png', __FILE__) );
+
+	//call register settings function
+	add_action( 'admin_init', 'register_exhibitors_system_settings' );
+}
+
+
+function register_exhibitors_system_settings() {
+	//register our settings
+	register_setting( 'exhibitors-code-system-option', 'code-prefix' );
+	register_setting( 'exhibitors-code-system-option', 'code-list' );
+	register_setting( 'exhibitors-code-system-option', 'form-id' );
+}
+
+function exhibitors_options_page() {
+?>
+	<div class="wrap">
+		<h1>Exhibitors Code System - Ustawienia</h1>
+		<div class="info notice-success notice">
+			<p>Zanim zaczniesz ustawiać wtyczkę zastosuj się do instrukcji. Wtyczka działa jedynie z formularzami Gravity Forms.</p>
+		</div>
+		<div class="info notice-error notice">
+			<p>Wtyczka nie działa na szablonie AVADA!</p>
+		</div>
+		<div id="col-container">
+			<div id="col-left" class="postbox-container">
+				<div class="col-wrap">
+					<h2>Ustawienia</h2>
+					<div class="form-wrap">
+						<form method="post" action="options.php">
+							<?php settings_fields( 'exhibitors-code-system-option' ); ?>
+							<?php do_settings_sections( 'exhibitors-code-system-option' ); ?>
+							<div class="form-field">
+								<label>Prefix dla generowanych kodów (np. <code>'BIO2019'</code>)</label>
+								<input type="text" name="code-prefix" value="<?php echo esc_attr( get_option('code-prefix') ); ?>" />
+								<p>Prefix do generowania nowych kodów dla wystawców.<br> Działanie: KOD123__ ; gdzie '__' numer wystawcy z kolei liczony od zera.</p>
+							</div>
+							<div class="form-field">
+								<label>Lista kodów istniejących wystawców (zarejestrowanych przed instalacją tej wtyczki)</label>
+								<input type="text" name="code-list" value="<?php echo esc_attr( get_option('code-list') ); ?>" />
+								<p>Odziel kody przecinkami, <strong>ostatni kod bez przecinka na końcu</strong> przykład: <code>XXX,YYY,ZZZ</code> lub zostaw puste.</p>
+							</div>
+							<div class="form-field">
+								<label>ID formularza rejestrującego wystawcę i nadającego mu kod zaproszenia</label>
+								<input type="text" name="form-id" value="<?php echo esc_attr( get_option('form-id') ); ?>" />
+								<p>ID formularza Gravity Forms który generuje kod wystawcy.</p>
+							</div>			
+							<?php submit_button(); ?>
+						</form>
+					</div>
+				</div>	
+			</div>
+			<div id="col-right">
+				<div class="col-wrap"><h2>Instrukcja</h3>
+					<div class="postbox-container" style="width: 100%">
+						<div class="postbox">
+							<div class="inside">
+								<div class="main">
+									<p><strong>O wtyczce:</strong><br>
+									Wtyczka umożliwa <strong>automatyczne generowanie kodów zaproszeń</strong> dla wystawców, który później <strong>zostaje wryfikowany przy rejestracji</strong> osoby zaproszonej. Dla uprzednio zarejestrowanych wystawców jest możliwośc dodania własnej <strong><em>(nieograniczonej znakowo)</em> puli kodów</strong>, która również zostaje weryfikowana podczas rejestracji osoby zaproszonej.</p>
+									<ol>
+										<li>Dodanie do <strong>formularza wystawcy</strong> pola <code>type="text"</code> o klasie <code>code</code>,</li>
+										<li>Załączenie tego pola oraz linka do odpowiedniej podstrony w mailu potwierdzającym <strong>dla wystawcy</strong>,</li>
+										<li>Dodanie do <strong>formularza rejestracji dla użytkowników</strong> pola <code>type="text"</code> o klasie <code>invitation_code</code>,</li>
+										<li>Załączenie tego pola w mailu potwierdzającym <strong>dla nas</strong>,</li>
+										<li>Ustawienie <strong>szablonu podstrony z rejestracją wystawców</strong> na <code>Exhibitors Code Maker</code></li>
+										<li>Ustawienie <strong>szablonu podstrony z rejestracją odwiedzających</strong> na <code>Exhibitors Code Checker</code></li>
+										<li>Uzupełnienie ustawień wtyczki odpowiednimi danymi.</li>
+									</ol>
+									<div style="text-align:right">
+										<img src="<?php echo plugins_url('icon.png', __FILE__)?>" alt="logo" >
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php } ?>
---
+++ page-checkform.php
@@ -0,0 +1,103 @@
+<?php
+	// Exhibitors Codes List
+	$existExhibitors = explode(', ', get_option('code-list'));
+	// JavaScript Shortcode
+	function styles_shortcode() {
+		ob_start();
+		if(!empty($_GET["input_value"])){ ?>
+			<script type="text/javascript">
+				document.querySelector("li.invitation_code div.ginput_container input").value = '<?= $_GET["input_value"] ?>';
+				document.querySelector("li.invitation_code").style.display = 'none';
+			</script>
+		<?php }
+		return ob_get_clean();
+	}
+	add_shortcode( 'styles', 'styles_shortcode' );
+	// Check Form Shortcode
+	function form_shortcode() {
+		ob_start(); 
+		get_header(); ?>
+		<style>
+			#check_user {
+				height: 164px;
+				width: 300px;
+				position: absolute;
+				left: calc(50% - 150px);
+				border-radius: 10px;
+				border-color: transparent;
+				margin: 30px auto;
+				text-align:center;
+			}
+			#check_user > input{
+				margin-top: 22%;
+				width: 95%;
+				margin-left: auto;
+				margin-right: auto;
+				margin-bottom: 20px;
+				-webkit-box-shadow: 5px 5px 10px 0px rgba(221,221,221,1);
+				-moz-box-shadow: 5px 5px 10px 0px rgba(221,221,221,1);
+				box-shadow: 5px 5px 10px 0px rgba(221,221,221,1);
+				border: solid 1px #ccc;
+			}
+		</style>
+			<div class="container relative text-center form-check-container">
+			<h1>Wpisz kod zaproszenia, który otrzymałeś od wystawcy</h1>
+			<h3>Zostaniesz przekierowany wtedy do formularza rejestracji</h3>
+				<form id="check_user" method="GET" action="">
+					<input type="text" id="input_value" name="input_value" />
+					<button type="submit" class="btn btn-success" name="save" id="save">WYŚLIJ</button>
+				</form>
+			</div>
+		<?php 
+		if ((!empty($_GET["input_value"]) && !mysqli_num_rows(mysqli_query($connect, $check_code_query)) > 0) || (!empty($_GET["input_value"]) && !in_array($passcode, $existExhibitors))){
+			echo "<h2 class='text-center text-danger mb-5'>Błędny kod</h2>";
+		}
+		get_footer();
+		return ob_get_clean();
+	}
+	add_shortcode( 'form_check', 'form_shortcode' );
+
+	// include PHP file
+	function PHP_Include($params = array()) {
+		extract(shortcode_atts(array(
+			'file' => 'default'
+		), $params));
+		ob_start();
+		include(get_theme_root() . '/' . get_template() . "/$file.php");
+		return ob_get_clean();
+	}
+	// register shortcode
+	add_shortcode('phpinclude', 'PHP_Include');
+
+	// If Clean $_GET request, do form shortcode
+	if (empty($_GET["input_value"])){
+	echo do_shortcode('[form_check]');
+	}
+	// If not clean $_GET request decide what to do
+	if (!empty($_GET["input_value"])){
+		$passcode = $_GET['input_value'];
+		$check_code_query = "SELECT * FROM `wp_gf_entry_meta` WHERE `meta_value` = '$passcode'";
+		require_once(ABSPATH . 'wp-config.php');
+		$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		// Check connect
+		if (mysqli_connect_errno()) {
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+		if (mysqli_num_rows(mysqli_query($connect, $check_code_query)) > 0 || in_array($passcode, $existExhibitors)) {
+
+		// FILTER CONTENT
+		function add_additional_php_code($content) {
+			$original = $content;
+			$content .= $original;
+			$content .= do_shortcode('[styles]');
+			return $content;
+			}
+		add_filter( 'the_content', 'add_additional_php_code'); 
+
+		// ECHO PAGE SHORTCODE 
+		echo do_shortcode("[phpinclude file='page']");	
+		}else{
+			echo do_shortcode('[form_check]');
+		}
+	}
+?>
